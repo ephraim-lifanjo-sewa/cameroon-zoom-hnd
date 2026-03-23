d9b177e5b76e96040a0d5e26f04e3ebeafbf1da2
ephraim lifanjo <ephraimlifanjos@gmail.com>
2026-03-23 17:54:28 +0100

    fix: runtime crash

--- src/components/search/MapBusinessCard.tsx
+++ src/components/search/MapBusinessCard.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { Badge } from '@/components/ui/badge';
+import "leaflet/dist/leaflet.css";
 import { cn } from '@/lib/utils';
 
 // ----- Types directly in this file -----
